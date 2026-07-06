672c6b014ac84ea7e061a73b529c88f919434f3a
vaddinandini2226 <vaddinandini2004@gmail.com>
2026-07-06 12:35:28 +0530

    [Nandini]: Implemented insertion, inorder traversal, and size in BST

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -2,13 +2,14 @@
  * Program: Binary Search Tree Implementation using Java
  * Description:
  * This program implements a Binary Search Tree (BST) using generics. It supports
- * inserting elements into the tree and performing an inorder traversal, which
- * displays the elements in sorted order according to the Binary Search Tree property.
+ * inserting elements into the tree, performing an inorder traversal to display
+ * elements in sorted order, and calculating the total number of nodes present
+ * in the tree.
  */
 package datastructures;
 public class BinarySearchTree<T extends Comparable<T>> {
-
     class Node {
+
         T data;
         Node left;
         Node right;
@@ -25,20 +26,21 @@ public class BinarySearchTree<T extends Comparable<T>> {
     public void add(T data) {
         root = addRecursive(root, data);
     }
+
     private Node addRecursive(Node root, T data) {
 
-        if (root == null) {
+        if (root == null)
             return new Node(data);
-        }
 
-        if (data.compareTo(root.data) < 0) {
+        if (data.compareTo(root.data) < 0)
             root.left = addRecursive(root.left, data);
-        } else if (data.compareTo(root.data) > 0) {
+
+        else if (data.compareTo(root.data) > 0)
             root.right = addRecursive(root.right, data);
-        }
 
         return root;
     }
+
     public void inorder(Node root) {
 
         if (root != null) {
@@ -47,13 +49,36 @@ public class BinarySearchTree<T extends Comparable<T>> {
             inorder(root.right);
         }
     }
+
+    public int size(Node root) {
+
+        if (root == null)
+            return 0;
+
+        return 1 + size(root.left) + size(root.right);
+    }
+
     public static void main(String[] args) {
 
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
         bst.add(56);
         bst.add(30);
         bst.add(70);
+        bst.add(22);
+        bst.add(40);
+        bst.add(60);
+        bst.add(95);
+        bst.add(11);
+        bst.add(65);
+        bst.add(3);
+        bst.add(16);
+        bst.add(63);
+        bst.add(67);
+
         System.out.println("Inorder Traversal:");
         bst.inorder(bst.root);
+        System.out.println("\n");
+        System.out.println("Size of Binary Tree : " + bst.size(bst.root));
     }
 }
